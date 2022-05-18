7b4d7b82c469f914edcbb5d9ed8e35b6f30be2a4
ranfys.valle@gmail.com <ranfys.valle@gmail.com>
2022-05-17 20:55:10 -0400

    modify leaderboard to do sorting using aggregation framework

--- functions/mdbw_leaderboard.js
+++ functions/mdbw_leaderboard.js
@@ -1,16 +1,17 @@
-// This function is the endpoint's request handler.
 exports = async function({ query, headers, body}, response) {
-    let votes = context.services.get("mongodb-atlas").db("mdbw").collection("votes");
-    let allVotes = await votes.aggregate([
-      {$lookup:
-        {
-          from: 'custom_data',
-          localField: 'email',
-          foreignField: 'email',
-          as: 'cd'
-        }
-      }  
-    ]).toArray();
-    
-    return allVotes;
+  let votes = context.services.get("mongodb-atlas").db("mdbw").collection("votes");
+  let allVotes = await votes.aggregate([
+    {$lookup:
+      {
+        from: 'custom_data',
+        localField: 'email',
+        foreignField: 'email',
+        as: 'cd'
+      }
+    },
+    {$project:{ voters_count: {$size: { "$ifNull": [ "$voters", [] ] } } }},
+    {$sort:{voters_count:1}}
+  ]).toArray();
+  
+  return allVotes;
 };
